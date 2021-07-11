f788d4bd28f1ceb2d99c519aa55514886fb61355
Eric <eric@example.com>
2021-07-11 17:02:26 +0800

    Update swagger-ui http basic auth default password

--- src/main/java/me/dodowhat/example/config/security/SwaggerConstants.java
+++ src/main/java/me/dodowhat/example/config/security/SwaggerConstants.java
@@ -3,7 +3,7 @@ package me.dodowhat.example.config.security;
 public class SwaggerConstants {
     public static final String SWAGGER_USERNAME = "swagger";
     public static final String PASSWORD_ENV = "SWAGGER_PASSWORD";
-    public static final String DEFAULT_PASSWORD = "password";
+    public static final String DEFAULT_PASSWORD = "swagger";
 
     public static String GetSwaggerPassword() {
         String password = System.getenv(PASSWORD_ENV);
